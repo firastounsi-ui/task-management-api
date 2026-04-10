16b7dc9a698cba0b626167b085d4c12d6aa1c9ae
Firas Tounsi <buz15qim@uni-duesseldorf.de>
2026-04-10 22:20:13 +0200

    Remove temporary password hash generator

--- src/main/java/com/example/taskapi/PasswordHashGenerator.java
+++
@@ -1,1 +0,0 @@
-package com.example.taskapi; import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; public class PasswordHashGenerator { public static void main(String[] args) { System.out.println(new BCryptPasswordEncoder().encode("admin123"));System.out.println(new BCryptPasswordEncoder().encode("admin123")); } }
